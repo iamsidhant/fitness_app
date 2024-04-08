e017f32409b088be88617ad5d17f996cb16011dd
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-04-08 11:50:30 +0530

    added navbar and Home component

--- fitness_app/src/App.js
+++ fitness_app/src/App.js
@@ -4,17 +4,17 @@ import { Box } from "@mui/material";
 
 import './App.css';
 import Home from "./pages/Home";
-import ExcerciseDetail from "./pages/ExcerciseDetail";
+import ExerciseDetail from "./pages/ExerciseDetail";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: '1448px' }}} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/excercise/:id" element={<ExcerciseDetail />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
       <Footer />
     </Box>
--- fitness_app/src/pages/Home.js
+++ fitness_app/src/pages/Home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Box } from '@mui/material';
+import { useState } from 'react';
+import HeroBanner from '../components/HeroBanner';
+import Exercises from '../components/Exercises';
 
 const Home = () => {
+    const [exercises, setExercises] = useState([]);
+    const [bodyPart, setBodyPart] = useState('all');
+
   return (
-    <div>Home</div>
+    <Box>
+        <HeroBanner />
+        <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
   )
 }
 
-export default Home
+export default Home;
